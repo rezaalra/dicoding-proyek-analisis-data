81dc15db90b958255ec0ba71fec9dc7ad85494f7
rezaalra <43606137+rezaalra@users.noreply.github.com>
2025-01-24 17:51:38 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,14 @@
-import streamlit as st
+import gdown
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+st.set_page_config(page_title="Dashboard E-commerce by Reza", page_icon=":bar_chart", layout="wide")
+st.title(" :bar_chart: Dashboard E-commerce by Reza :bar_chart:")
+st.header("Dashboard E-Commerce")
+st.subheader("Muhamad Reza Al Ramadhan, rezaalramadhan@gmail.com")
+st.subheader("ID: https://www.dicoding.com/users/reza_al_ramadhan/")
 
 # Placeholder: Simulasi data berdasarkan insight yang ada
 # Data penjualan berdasarkan kategori
