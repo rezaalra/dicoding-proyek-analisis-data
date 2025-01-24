a70d319b7ac18c9c9f589f82f625933f9fb8de43
rezaalra <43606137+rezaalra@users.noreply.github.com>
2025-01-24 16:57:51 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,8 +4,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
-st.set_page_config(page_title="Dashboard E-commerce by Yohana", page_icon=":bar_chart", layout="wide")
-st.title(" :bar_chart: Dashboard E-commerce by Yohana :bar_chart:")
+st.set_page_config(
+    page_title="Dashboard E-commerce by Reza",
+    page_icon=":bar_chart", layout="wide")
+st.title(":bar_chart: Dashboard E-commerce by Reza")
 st.header("Dashboard E-Commerce")
-st.subheader("Yohana Beatrice Nainggolan, yohanabeatrice1263@gmail.com")
-st.subheader("ID: yohanabeatrice")
+st.subheader("Muhamad Reza Al Ramadhan, rezaalramadhan@gmail.com")
+st.subheader("ID: https://www.dicoding.com/users/reza_al_ramadhan/")
