3e6d8fd6f69a5bda23741e5ef13d845a05dbc548
rezaalra <43606137+rezaalra@users.noreply.github.com>
2025-01-27 19:52:22 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -59,6 +59,10 @@ integer_columns = ['product_name_lenght',
 products_df = products_df.astype(
     {column:'int' for column in integer_columns})
 
+# Membersihkan tabel order_payments_df
+order_payments_df['payment_installments'] = \
+  order_payments_df['payment_installments'].replace(0, 1)
+
 # Join semua data yang perlukan
 order_products_df = pd.merge(order_items_df,
                              products_df,
@@ -76,9 +80,124 @@ customer_order_items_df = pd.merge(customer_order_items_df,
                                    right_on='order_id',
                                    how='inner')
 
+def product_order_counts_pivot():
+    product_order_counts = order_products_df.groupby(
+        by="product_category_name").order_id.count()
+    product_order_counts.rename("order_count", inplace=True)
+    return product_order_counts
 
-def show_best_selling_products():
-    st.write("Error")
+def order_items_by_city_pivot():
+    order_items_by_city = customer_order_items_df.groupby(
+        by="customer_city").order_item_id.count().sort_values(ascending=False)
+    order_items_by_city.rename("order_count", inplace=True)
+    return order_items_by_city
+
+def order_items_by_state_pivot():
+    order_items_by_state = customer_order_items_df.groupby(
+        by="customer_state").order_item_id.count().sort_values(ascending=False)
+    order_items_by_state.rename("order_count", inplace=True)
+    return order_items_by_state
+
+def payment_type_counts_pivot()
+    payment_type_counts = order_payments_df.groupby(
+        by="payment_type").order_id.count().sort_values(ascending=False)
+    payment_type_counts.rename("order_count", inplace=True)
+    return payment_type_counts
+
+def payment_installment_counts_pivot():
+    payment_installment_counts = order_payments_df.groupby(
+        ["payment_type", 'payment_installments']).order_id.count()
+    payment_installment_counts.rename("order_count", inplace=True)
+    return payment_installment_counts
+
+# Visualisasi Penjualan terbaik Berdasarkan Kategori Produk
+def show_best_selling_products(best_selling_products):
+    st.header("Best Selling Products")
+    fig1, ax1 = plt.subplots(figsize=(8, 4))
+    colors = ["#00FF57"] + ["#D3D3D3"] * (len(best_selling_products) - 1)
+    sns.barplot(
+        data=best_selling_products,
+        x="order_count",
+        y="product_category_name",
+        ax=ax1,
+        palette=colors)
+    ax1.set_xlabel("Sales")
+    ax1.set_ylabel("Category")
+    st.pyplot(fig1)
+
+# Visualisasi Penjualan terburuk Berdasarkan Kategori Produk
+def show_worst_selling_products(worst_selling_products):
+    st.header("Worst Selling Products")
+    fig2, ax2 = plt.subplots(figsize=(8, 4))
+    colors = ["#F90611"] + ["#D3D3D3"] * (len(worst_selling_products) - 1)
+    sns.barplot(
+        data=worst_selling_products,
+        x="order_count",
+        y="product_category_name",
+        ax=ax2,
+        palette=colors)
+    ax2.set_xlabel("Sales")
+    ax2.set_ylabel("Category")
+    ax2.invert_xaxis()
+    ax2.yaxis.set_label_position("right")
+    ax2.yaxis.tick_right()
+    st.pyplot(fig2)
+
+# Visualisasi Penjualan Berdasarkan Wilayah Kota Customer
+def show_city_sales(city_sales_df):
+    st.header("Number of Orders by City")
+    fig3, ax3 = plt.subplots(figsize=(8, 4))
+    colors = ["#00FF57"] + ["#D3D3D3"] * (len(city_sales_df) - 1)
+    sns.barplot(
+        data=city_sales_df,
+        x="order_count",
+        y="customer_city",
+        ax=ax3,
+        palette=colors)
+    ax3.set_xlabel("Sales")
+    ax3.set_ylabel("City")
+    st.pyplot(fig3)
+
+# Visualisasi Penjualan Berdasarkan Wilayah State Customer
+def show_city_sales(state_sales_df):
+    st.header("Number of Orders by State")
+    fig4, ax4 = plt.subplots(figsize=(8, 4))
+    colors = ["#00FF57"] + ["#D3D3D3"] * (len(state_sales_df) - 1)
+    sns.barplot(
+        data=state_sales_df,
+        x="order_count",
+        y="customer_state",
+        ax=ax4,
+        palette=colors)
+    ax4.set_xlabel("Sales")
+    ax4.set_ylabel("City")
+    st.pyplot(fig4)
+
+# Visualisasi Sebaran Metode Pembayaran
+def show_payment_methods(payment_methods_df):
+    st.header("Number of Customers by Payment Method")
+    fig5, ax5 = plt.subplots(figsize=(8, 4))
+    colors = ["#00FF57"] + ["#D3D3D3"] * (len(payment_methods_df) - 1)
+    sns.barplot(
+        data=payment_methods_df,
+        x="order_count",
+        y="payment_type",
+        ax=ax5,
+        palette=colors)
+    ax5.set_xlabel("Usage")
+    ax5.set_ylabel("Method")
+    st.pyplot(fig5)
+
+# Visualisasi Sebaran Metode Pembayaran
+def show_payment_methods(payment_installment_credit_card_plot):
+    st.header("Number of Customers by Number of Installments")
+    fig6, ax6 = plt.subplots(figsize=(8, 4))
+    plt.plot(payment_installment_credit_card_plot["payment_installments"],
+            payment_installment_credit_card_plot["order_count"])
+    plt.xticks(payment_installment_credit_card_plot["payment_installments"])
+    plt.xlabel("Number of Installments")
+    plt.ylabel("Number of Customers")
+    st.pyplot(fig6)
 
 # Header
 st.set_page_config(
@@ -105,136 +224,28 @@ end_date = st.date_input(
     max_value=last_date)
 
 if st.button("Apply"):
-    show_best_selling_products()
+    # Data penjualan berdasarkan kategori
+    product_order_counts = product_order_counts_pivot()
 
-# Exploratory Data Analysis (EDA)
-# Explore kategori produk berdasarkan jumlah penjualan yang telah terjadi
-product_order_counts = order_products_df.groupby(
-    by="product_category_name").order_id.count()
-product_order_counts.rename("order_count", inplace=True)
-product_order_counts = product_order_counts.sort_values(ascending=False)
+    # Data penjualan terbaik berdasarkan kategori
+    best_selling_products = product_order_counts.reset_index().head(10)
 
-# Explore jumlah penjualan yang telah terjadi berdasarkan wilayahnya
-# Explore jumlah penjualan yang telah terjadi berdasarkan kotanya
-order_items_by_city = customer_order_items_df.groupby(
-    by="customer_city").order_item_id.count().sort_values(ascending=False)
-order_items_by_city.rename("order_count", inplace=True)
+    # Data penjualan terburuk berdasarkan kategori
+    worst_selling_products = product_order_counts.reset_index().tail(10)[::-1]
 
-# Explore jumlah penjualan yang telah terjadi berdasarkan statenya
-order_items_by_state = customer_order_items_df.groupby(
-    by="customer_state").order_item_id.count().sort_values(ascending=False)
-order_items_by_state.rename("order_count", inplace=True)
+    # Data penjualan berdasarkan wilayah (kota)
+    city_sales_df = order_items_by_city_pivot().reset_index().head(10)
 
-# Explore sebaran metode pembayaran yang digunakan dalam data transaksi customer
-order_payments_df['payment_installments'] = \
-  order_payments_df['payment_installments'].replace(0, 1)
+    # Data penjualan berdasarkan wilayah (state)
+    state_sales_df = order_items_by_state_pivot().reset_index().head(10)
 
-payment_type_counts = order_payments_df.groupby(
-    by="payment_type").order_id.count()
-payment_type_counts.rename("order_count", inplace=True)
-payment_type_counts = payment_type_counts.sort_values(ascending=False)
-payment_installment_counts = order_payments_df.groupby(
-    ["payment_type", 'payment_installments']).order_id.count()
-payment_installment_counts.rename("order_count", inplace=True)
+    # Data metode pembayaran
+    payment_methods_df = payment_type_counts_pivot().reset_index()
 
-# Data penjualan terbaik berdasarkan kategori
-best_selling_products = product_order_counts.reset_index().head(10)
+    # Data metode pembayaran berdasarkan cicilan credit card
+    payment_installment_credit_card_plot = \
+        payment_installment_counts_pivot()['credit_card'].reset_index()
 
-# Data penjualan terburuk berdasarkan kategori
-worst_selling_products = product_order_counts.reset_index().tail(10)[::-1]
-
-# Data penjualan berdasarkan wilayah (kota)
-city_sales_df = order_items_by_city.reset_index().head(10)
-
-# Data penjualan berdasarkan wilayah (state)
-state_sales_df = order_items_by_state.reset_index().head(10)
-
-# Data metode pembayaran
-payment_methods_df = payment_type_counts.reset_index()
-
-# Data metode pembayaran berdasarkan cicilan credit card
-payment_installment_credit_card_plot = \
-  payment_installment_counts['credit_card'].reset_index()
-
-# Visualisasi Penjualan terbaik Berdasarkan Kategori Produk
-st.header("Best Selling Products")
-fig1, ax1 = plt.subplots(figsize=(8, 4))
-colors = ["#00FF57"] + ["#D3D3D3"] * (len(best_selling_products) - 1)
-sns.barplot(
-    data=best_selling_products,
-    x="order_count",
-    y="product_category_name",
-    ax=ax1,
-    palette=colors)
-ax1.set_xlabel("Sales")
-ax1.set_ylabel("Category")
-st.pyplot(fig1)
-
-# Visualisasi Penjualan terburuk Berdasarkan Kategori Produk
-st.header("Worst Selling Products")
-fig2, ax2 = plt.subplots(figsize=(8, 4))
-colors = ["#F90611"] + ["#D3D3D3"] * (len(worst_selling_products) - 1)
-sns.barplot(
-    data=worst_selling_products,
-    x="order_count",
-    y="product_category_name",
-    ax=ax2,
-    palette=colors)
-ax2.set_xlabel("Sales")
-ax2.set_ylabel("Category")
-ax2.invert_xaxis()
-ax2.yaxis.set_label_position("right")
-ax2.yaxis.tick_right()
-st.pyplot(fig2)
-
-# Visualisasi Penjualan Berdasarkan Wilayah Kota Customer
-st.header("Number of Orders by City")
-fig3, ax3 = plt.subplots(figsize=(8, 4))
-colors = ["#00FF57"] + ["#D3D3D3"] * (len(city_sales_df) - 1)
-sns.barplot(
-    data=city_sales_df,
-    x="order_count",
-    y="customer_city",
-    ax=ax3,
-    palette=colors)
-ax3.set_xlabel("Sales")
-ax3.set_ylabel("City")
-st.pyplot(fig3)
-
-# Visualisasi Penjualan Berdasarkan Wilayah State Customer
-st.header("Number of Orders by State")
-fig4, ax4 = plt.subplots(figsize=(8, 4))
-colors = ["#00FF57"] + ["#D3D3D3"] * (len(state_sales_df) - 1)
-sns.barplot(
-    data=state_sales_df,
-    x="order_count",
-    y="customer_state",
-    ax=ax4,
-    palette=colors)
-ax4.set_xlabel("Sales")
-ax4.set_ylabel("City")
-st.pyplot(fig4)
-
-# Visualisasi Sebaran Metode Pembayaran
-st.header("Number of Customers by Payment Method")
-fig5, ax5 = plt.subplots(figsize=(8, 4))
-colors = ["#00FF57"] + ["#D3D3D3"] * (len(payment_methods_df) - 1)
-sns.barplot(
-    data=payment_methods_df,
-    x="order_count",
-    y="payment_type",
-    ax=ax5,
-    palette=colors)
-ax5.set_xlabel("Usage")
-ax5.set_ylabel("Method")
-st.pyplot(fig5)
-
-# Visualisasi Sebaran Metode Pembayaran
-st.header("Number of Customers by Number of Installments")
-fig6, ax6 = plt.subplots(figsize=(8, 4))
-plt.plot(payment_installment_credit_card_plot["payment_installments"],
-         payment_installment_credit_card_plot["order_count"])
-plt.xticks(payment_installment_credit_card_plot["payment_installments"])
-plt.xlabel("Number of Installments")
-plt.ylabel("Number of Customers")
-st.pyplot(fig6)
+    # Visualisasi semua data
+    show_best_selling_products(best_selling_products)
+    show_worst_selling_products(worst_selling_products)
