b82ee9101e79328a1b449514c47f8ba5f889421c
rezaalra <43606137+rezaalra@users.noreply.github.com>
2025-01-27 19:20:26 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -76,36 +76,6 @@ customer_order_items_df = pd.merge(customer_order_items_df,
                                    right_on='order_id',
                                    how='inner')
 
-# Header
-st.set_page_config(
-    page_title="Dashboard E-commerce by Reza",
-    page_icon=":bar_chart",
-    layout="wide")
-st.title(":bar_chart: Dashboard E-commerce by Reza")
-st.subheader("Nama: Muhamad Reza Al Ramadhan")
-st.subheader("Email: rezaalramadhan@gmail.com")
-st.subheader("ID: https://www.dicoding.com/users/reza_al_ramadhan/")
-
-# Date Filter
-earliest_date = orders_df.order_purchase_timestamp.min()
-last_date  = orders_df.order_purchase_timestamp.max()
-start_date = st.date_input(
-    label="Start Date", 
-    value=None,
-    min_value=earliest_date,
-    max_value=last_date)
-end_date = st.date_input(
-    label="End Date",
-    value=None,
-    min_value=earliest_date,
-    max_value=last_date)
-st.write(end_date)
-if st.button("Apply"):
-    show_best_selling_products()
-
-def show_best_selling_products():
-    st.write("Error")
-
 # Exploratory Data Analysis (EDA)
 # Explore kategori produk berdasarkan jumlah penjualan yang telah terjadi
 product_order_counts = order_products_df.groupby(
@@ -237,3 +207,33 @@ plt.xticks(payment_installment_credit_card_plot["payment_installments"])
 plt.xlabel("Number of Installments")
 plt.ylabel("Number of Customers")
 st.pyplot(fig6)
+
+# Header
+st.set_page_config(
+    page_title="Dashboard E-commerce by Reza",
+    page_icon=":bar_chart",
+    layout="wide")
+st.title(":bar_chart: Dashboard E-commerce by Reza")
+st.subheader("Nama: Muhamad Reza Al Ramadhan")
+st.subheader("Email: rezaalramadhan@gmail.com")
+st.subheader("ID: https://www.dicoding.com/users/reza_al_ramadhan/")
+
+# Date Filter
+earliest_date = orders_df.order_purchase_timestamp.min()
+last_date  = orders_df.order_purchase_timestamp.max()
+start_date = st.date_input(
+    label="Start Date", 
+    value=None,
+    min_value=earliest_date,
+    max_value=last_date)
+end_date = st.date_input(
+    label="End Date",
+    value=None,
+    min_value=earliest_date,
+    max_value=last_date)
+st.write(end_date)
+if st.button("Apply"):
+    show_best_selling_products()
+
+def show_best_selling_products():
+    st.write("Error")
